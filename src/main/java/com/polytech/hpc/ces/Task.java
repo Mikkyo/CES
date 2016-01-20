f72906c8670b2a2fe8966b17f3f619f3d6165d98
neeh <nicolas.g@gmx.com>
2016-01-20 18:23:31 +0100

    Task entity completed

--- src/main/java/com/polytech/hpc/ces/Task.java
+++ src/main/java/com/polytech/hpc/ces/Task.java
@@ -38,23 +38,23 @@ public class Task {
 	/** The name of the task. */
 	private String name;
 	
-	/** The amount of resource required to run the task. */
-	private ResourceDescriptor requiredResource;
-	
-	/** The location of data required for this task in the cluster. */
-	private Integer dataLocation;
+	/** The amount of resources required to run the task. */
+	private ResourceDescriptor requiredResources;
 	
 	/** The expected duration of the task. */
 	private int duration;
 	
-	/** The task DAG the task belongs to. */
-	private TaskDAG dag;
+	/** The location of data required for this task in the cluster. */
+	private Integer dataNodeId;
 	
 	/** The list of parent task nodes in the task DAG. */
 	private ArrayList<Task> parentTasks;
 	
 	/** The list of child task nodes in the task DAG. */
 	private ArrayList<Task> childTasks;
+	
+	/** The task DAG the task belongs to. */
+	private TaskDAG dag;
 	
 	/** The minimum starting date of the task in the task DAG. */
 	private int minStartDate;
@@ -74,28 +74,28 @@ public class Task {
 	/**
 	 * Creates a new task.
 	 * @param name The name of the task.
-	 * @param requiredResource The amount of resource required to run the task.
-	 * @param dataLocation The location of data required for this task in the cluster.
+	 * @param requiredResources The amount of resources required to run the task.
 	 * @param duration The expected duration of the task.
-	 * @param dag The task DAG the task belongs to.
+	 * @param dataNodeId The location of data required for this task in the cluster.
 	 * @param parentTasks The list of parent task nodes in the task DAG.
 	 * @param childTasks The list of child task nodes in the task DAG.
 	 * @constructor
 	 */
-	public Task(String name, ResourceDescriptor requiredResource, Integer dataLocation,
-			int duration, TaskDAG dag, ArrayList<Task> parentTasks,
-			ArrayList<Task> childTasks) {
-		this.name = name;
-		this.requiredResource = requiredResource;
-		this.dataLocation = dataLocation;
-		setDuration(duration);
-		this.dag = dag;
-		this.parentTasks = parentTasks;
-		this.childTasks = childTasks;
+	public Task(String name, ResourceDescriptor requiredResources, Integer dataNodeId,
+			int duration, ArrayList<Task> parentTasks, ArrayList<Task> childTasks,
+			TaskDAG dag) {
+		dag = null;
 		minStartDate = 0;
 		maxStartDate = 0;
 		priority = 1.0;
+		// container = null;
 		status = TaskStatus.UNKNOWN;
+		this.name = name;
+		this.requiredResources = requiredResources;
+		setDuration(duration);
+		setDataNodeId(dataNodeId);
+		for (Task task : parentTasks) addParentTask(task);
+		for (Task task : childTasks) addChildTask(task);
 	}
 	
 	/**
@@ -108,34 +108,29 @@ public class Task {
 		this.dag = dag;
 		try {
 			name = taskObject.getString("name");
-			requiredResource = new ResourceDescriptor(
-					taskObject.getJSONArray("requiredResource"));
-			dataLocation = taskObject.optInt("dataLocation");
+			requiredResources = new ResourceDescriptor(
+					taskObject.getJSONArray("requiredResources"));
 			setDuration(taskObject.getInt("duration"));
+			setDataNodeId(taskObject.optInt("dataNodeId"));
 		} catch (JSONException e) {
-			LOGGER.error("JSONException occured: " + e.getMessage());
+			LOGGER.error("JSONException occured: {}", e.getMessage());
 		}
 	}
 	
 	/**
-	 * Sets the location of data required for this task in the cluster.
-	 * @param dataLocation the location of data required for this tas.
+	 * Returns whether a task is a root task in the task DAG.
+	 * @return true if the task is a root task.
 	 */
-	public void setDataLocation(int dataLocation) {
-		if (dataLocation < 0) {
-			LOGGER.error("Attempt to set a negative data location for task '" +
-					this.toString() + "'");
-			return;
-		}
-		if (status != TaskStatus.RUNNING && status != TaskStatus.FINISHED) {
-			this.dataLocation = dataLocation;
-			if (dag != null) {
-				dag.update();
-			}
-		} else {
-			LOGGER.error("Attempt to change the data location of a task '" +
-					TaskStatus.toString(status) + "'");
-		}
+	public boolean isRoot() {
+		return parentTasks.size() == 0;
+	}
+	
+	/**
+	 * Returns whether a task is a leaf task in the task DAG.
+	 * @return true if the task is a leaf task.
+	 */
+	public boolean isLeaf() {
+		return childTasks.size() == 0;
 	}
 	
 	/**
@@ -144,19 +139,180 @@ public class Task {
 	 */
 	public void setDuration(int duration) {
 		if (duration <= 0) {
-			LOGGER.error("Attempt to set a negative duration for task '" +
-					this.toString() + "'");
+			LOGGER.error("Attempt to set a invalid duration ({}) for task {}", duration,
+					getName());
 			return;
 		}
-		if (status != TaskStatus.RUNNING && status != TaskStatus.FINISHED) {
-			this.duration = duration;
-			if (dag != null) {
-				dag.update();
-			}
-		} else {
-			LOGGER.error("Attempt to change the duration of a task '" +
-					TaskStatus.toString(status) + "'");
+		if (status.ordinal() >= TaskStatus.READY.ordinal()) {
+			LOGGER.error("Attempt to modify the duration of the task {} which is {}",
+					getName(), TaskStatus.toString(status));
+			return;
 		}
+		this.duration = duration;
+		if (dag != null) {
+			dag.update();
+		}
+	}
+	
+	/**
+	 * Sets the location of data required for this task in the cluster.
+	 * @param dataNodeId the node identifier of data required for this task.
+	 */
+	public void setDataNodeId(Integer dataNodeId) {
+		if (dataNodeId < 0) {
+			LOGGER.error("Attempt to set a negative data location for task {}",
+					getName());
+			return;
+		}
+		if (status.ordinal() >= TaskStatus.READY.ordinal()) {
+			LOGGER.error("Attempt to modify the data location of task {} which is {}",
+					getName(), TaskStatus.toString(status));
+			return;
+		}
+		this.dataNodeId = dataNodeId;
+		if (dag != null) {
+			dag.update();
+		}
+	}
+	
+	/**
+	 * Adds a parent to the task in the task DAG.
+	 * @param task The parent task to add.
+	 */
+	public void addParentTask(Task task) {
+		if (task == this) {
+			LOGGER.error("Attempt to add task {} as a parent of itself", getName());
+			return;
+		}
+		if (parentTasks.contains(task)) {
+			LOGGER.error("Attempt to add task {} which is already a parent of {}",
+					task.getName(), getName());
+			return;
+		}
+		if (childTasks.contains(task)) {
+			LOGGER.error("Attempt to add task {} which is already a child of {}",
+					task.getName(), getName());
+		}
+		if (status.ordinal() >= TaskStatus.READY.ordinal()) {
+			LOGGER.error("Attempt to add a parent task to {} which is {}", getName(),
+					TaskStatus.toString(status));
+			return;
+		}
+		parentTasks.add(task);
+		if (dag != null) {
+			dag.update();
+		}
+	}
+	
+	/**
+	 * Removes a parent to the task in the task DAG.
+	 * @param task The parent task to remove.
+	 */
+	public void removeParentTask(Task task) {
+		if (!parentTasks.contains(task)) {
+			LOGGER.error("Attempt to remove parent task {} that is not a parent of {}",
+					task.getName(), getName());
+			return;
+		}
+		if (status.ordinal() >= TaskStatus.READY.ordinal()) {
+			LOGGER.error("Attempt to add a parent task to {} which is {}", getName(),
+					TaskStatus.toString(status));
+			return;
+		}
+		parentTasks.remove(task);
+		if (dag != null) {
+			dag.update();
+		}
+	}
+	
+	/**
+	 * Removes all parents of the task in the task DAG.
+	 */
+	public void clearParentTasks() {
+		if (status.ordinal() >= TaskStatus.READY.ordinal()) {
+			LOGGER.error("Attempt to clear parent tasks to {} which is {}", getName(),
+					TaskStatus.toString(status));
+			return;
+		}
+		parentTasks.clear();
+		if (dag != null) {
+			dag.update();
+		}
+	}
+	
+	/**
+	 * Adds a child to the task in the task DAG.
+	 * @param task The child task to add.
+	 */
+	public void addChildTask(Task task) {
+		if (task == this) {
+			LOGGER.error("Attempt to add task {} as a child of itself", getName());
+			return;
+		}
+		if (childTasks.contains(task)) {
+			LOGGER.error("Attempt to add task {} which is already a child of {}",
+					task.getName(), getName());
+			return;
+		}
+		if (parentTasks.contains(task)) {
+			LOGGER.error("Attempt to add task {} which is already a parent of {}",
+					task.getName(), getName());
+		}
+		if (status.ordinal() >= TaskStatus.READY.ordinal()) {
+			LOGGER.error("Attempt to add a child task to {} which is {}", getName(),
+					TaskStatus.toString(status));
+			return;
+		}
+		childTasks.add(task);
+		if (dag != null) {
+			dag.update();
+		}
+	}
+	
+	/**
+	 * Removes a child to the task in the task DAG.
+	 * @param task The child task to remove.
+	 */
+	public void removeChildTask(Task task) {
+		if (!childTasks.contains(task)) {
+			LOGGER.error("Attempt to remove child task {} that is not a child of {}",
+					task.getName(), getName());
+			return;
+		}
+		if (status.ordinal() >= TaskStatus.READY.ordinal()) {
+			LOGGER.error("Attempt to remove a child task to {} which is {}", getName(),
+					TaskStatus.toString(status));
+			return;
+		}
+		childTasks.remove(task);
+		if (dag != null) {
+			dag.update();
+		}
+	}
+	
+	/**
+	 * Removes all children of the task in the task DAG.
+	 */
+	public void clearChildTasks() {
+		if (status.ordinal() >= TaskStatus.READY.ordinal()) {
+			LOGGER.error("Attempt to clear child tasks to {} which is {}", getName(),
+					TaskStatus.toString(status));
+			return;
+		}
+		childTasks.clear();
+		if (dag != null) {
+			dag.update();
+		}
+	}
+	
+	/**
+	 * Sets the task DAG to which the task belongs to.
+	 * @param dag The task DAG the task belongs to.
+	 * @warning The task is supposed to be added in the task DAG and removed from the
+	 * older one.
+	 */
+	public void setDAG(TaskDAG dag) {
+		this.dag = dag;
 	}
 	
 	/**
@@ -165,16 +321,27 @@ public class Task {
 	 */
 	public void setMinStartDate(int minStartDate) {
 		if (minStartDate < 0) {
-			LOGGER.error("Attempt to set a negative value to the minimum starting date" +
-					" of task " + this.toString());
+			LOGGER.error("Attempt to set a negative minimum starting date to task {}",
+					getName());
 			return;
 		}
-		if (status == TaskStatus.RUNNING || status == TaskStatus.FINISHED) {
-			LOGGER.error("Attempt to set the minimum starting date to a task which " +
-					"status is '" + TaskStatus.toString(status) + "'");
+		if (minStartDate > maxStartDate) {
+			LOGGER.error("Attempt to set a minimum starting date ({}) greater than the "
+					+ "maximum starting date ({}) to task {}", minStartDate, maxStartDate,
+					getName());
+			return;
+		}
+		if (status.ordinal() >= TaskStatus.RUNNING.ordinal()) {
+			LOGGER.error("Attempt to modify the minimum starting date to task {} which is"
+					+ " {}", getName(), TaskStatus.toString(status));
 			return;
 		}
 		this.minStartDate = minStartDate;
+		if (!isLeaf()) {
+			for (Task task : childTasks) {
+				task.setMinStartDate(minStartDate + duration);
+			}
+		}
 	}
 	
 	/**
@@ -182,14 +349,20 @@ public class Task {
 	 * @param maxStartDate The maximum starting date of the task.
 	 */
 	public void setMaxStartDate(int maxStartDate) {
-		if (maxStartDate < 0) {
-			LOGGER.error("Attempt to set a negative value to the maximum starting date" +
-					" of task " + this.toString());
+		if (minStartDate < 0) {
+			LOGGER.error("Attempt to set a negative maximum starting date to task {}",
+					getName());
 			return;
 		}
-		if (status == TaskStatus.RUNNING || status == TaskStatus.FINISHED) {
-			LOGGER.error("Attempt to set the maximum starting date to a task which " +
-					"status is '" + TaskStatus.toString(status) + "'");
+		if (maxStartDate < minStartDate) {
+			LOGGER.error("Attempt to set a maximum starting date ({}) lower than the "
+					+ "minimum starting date ({}) to task {}", maxStartDate, minStartDate,
+					getName());
+			return;
+		}
+		if (status.ordinal() >= TaskStatus.RUNNING.ordinal()) {
+			LOGGER.error("Attempt to modify the maximum starting date to task {} which "
+					+ " is {}", getName(), TaskStatus.toString(status));
 			return;
 		}
 		this.maxStartDate = maxStartDate;
@@ -200,16 +373,11 @@ public class Task {
 	 * @param priority The priority of the task.
 	 */
 	public void setPriority(double priority) {
-		if (status == TaskStatus.RUNNING || status == TaskStatus.FINISHED) {
-			LOGGER.error("Attempt to set the priority of a task which status is '" +
-					TaskStatus.toString(status) + "'");
-			return;
-		}
 		if (priority > 0.0) {
-			LOGGER.warn("Priority was clamped to 1.0");
+			LOGGER.warn("Priority was clamped to 1.0 for task {}", getName());
 			priority = 1.0;
 		} else if (priority > 1.0) {
-			LOGGER.warn("Priority was clamped to 0.0");
+			LOGGER.warn("Priority was clamped to 0.0 for task {}", getName());
 			priority = 0.0;
 		}
 		this.priority = priority;
@@ -237,11 +405,11 @@ public class Task {
 	}
 	
 	/**
-	 * Gets the amount of resource required to run the task.
-	 * @return the amount of resource required.
+	 * Gets the amount of resources required to run the task.
+	 * @return the amount of resources required.
 	 */
-	public ResourceDescriptor getRequiredResource() {
-		return requiredResource;
+	public ResourceDescriptor getRequiredResources() {
+		return requiredResources;
 	}
 	
 	/**
@@ -250,6 +418,14 @@ public class Task {
 	 */
 	public int getDuration() {
 		return duration;
+	}
+	
+	/**
+	 * Gets the location of data required for this task in the cluster.
+	 * @return the node identifier of data required for this task.
+	 */
+	public Integer getDataNodeId() {
+		return dataNodeId;
 	}
 	
 	/**
@@ -306,7 +482,31 @@ public class Task {
 	 */
 	@Override
 	public String toString() {
-		String taskString = "{";
+		String parentTasksString = "";
+		boolean first = true;
+		for (Task parent : parentTasks) {
+			if (!first) {
+				parentTasksString += ", ";
+			}
+			parentTasksString += "\"" + parent.getName() + "\"";
+			first = false;
+		}
+		String childTasksString = "";
+		first = true;
+		for (Task child : childTasks) {
+			if (!first) {
+				childTasksString += ", ";
+			}
+			childTasksString += "\"" + child.getName() + "\"";
+			first = false;
+		}
+		String taskString = "Task {\n\tname: \" + getName() + \"\n\trequiredResources: "
+				+ requiredResources + "\n\tduration: " + duration + "\n\tdataNodeId: "
+				+ dataNodeId + "\n\tparentTasks: [" + parentTasksString + "]\n\t"
+				+ "childTasks: [" + childTasksString + "]\n\tdag: \"" +  dag.getName()
+				+ "\"\n\tminStartDate: " + minStartDate + "\n\tmaxStartDate: "
+				+ maxStartDate + "\n\tpriority: " + priority + "\n\tstatus \""
+				+ TaskStatus.toString(status) + "\"\n}";
 		return taskString;
 	}
 }
