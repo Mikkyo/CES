3f3f9fd90f9864229e6d6caa2c088a23ab0db70c
neeh <nicolas.g@gmx.com>
2016-01-21 22:42:29 +0100

    Added class 'Container'
    todo: implement this class

---
+++ src/main/java/com/polytech/hpc/ces/Container.java
@@ -0,0 +1,35 @@
+/* 
+ * This source file is part of CES.
+ * 
+ * Coyright(C) 2015 Nicolas Gougeon
+ * 
+ * CES is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * CES is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with HelloAnt.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.polytech.hpc.ces;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class represents a container used to run task.
+ * Containers are a subset of the node resources.
+ * @class
+ * @author Nicolas
+ */
+public class Container {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Container.class);
+	
+	// TODO: implement this class
+}
